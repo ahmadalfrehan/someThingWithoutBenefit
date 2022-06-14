a2418a98e2961a946f6a10274d4c886f2e7514ed
unknown <afrehan80@gmail.com>
2022-06-14 23:54:37 +0300

    aa

--- Z.pl
+++ Z.pl
@@ -44,3 +44,48 @@ wall_num(7,6,1).
 light(1,1).
 light(1,5).
 lght(1,7).
+
+
+cell(X,Y):-X>0,X<9,Y>0,Y<9.
+
+neighbors(X,Y,L):-cell(X,Y),
+cell(X+1,Y),X1 is X + 1,Y1 is Y  ,L =[X1,Y1];
+cell(X-1,Y),X1 is X - 1,Y1 is Y  ,L=[X1,Y1];
+cell(X,Y+1),X1 is X    ,Y1 is Y + 1 ,L=[X1,Y1];
+cell(X,Y-1),X1 is X    ,Y1 is Y - 1 ,L=[X1,Y1].
+
+yneighbors(X,Y,L):-cell(X,Y+1),L=[X,Y].
+
+grid_row(R,N,(R,N)) :-size(8,8),\+wall_num(R,N,_),\+wall(R,N).
+grid_row(R,N,Row) :-M is N + 1,cell(R, M),grid_row(R, M, Row).
+grid_row(R,N,Row) :- M is N-7,R1 is R + 1 ,cell(R1, M),grid_row(R1, M,Row).
+collect(R,N,M):-findall((Row),grid_row(R,N,Row),M).
+
+
+:- call1(......, L1), call2(.......,L2), append(L1,L2,L).
+
+
+cellRight(X,Y,L):-  X1 is X + 1, cell(X1,Y), not(wall(X1,Y)), not(wall_num(X1,Y,_)), cellRight(X1,Y,L1), append([[X1,Y]],L1,L).
+
+cellRight(_,_,[]).
+
+cellLeft(X,Y,L):-  X1 is X - 1, cell(X1,Y), not(wall(X1,Y)), not(wall_num(X1,Y,_)), cellLeft(X1,Y,L1), append([[X1,Y]],L1,L).
+
+cellLeft(_,_,[]).
+
+
+
+exRow(X,Y,L):- cellRight(X,Y,L1), cellLeft(X,Y,L2), append(L1,L2,L).
+
+
+
+cellUp(Y,X,L):- Y1 is Y + 1, cell(Y1,X), not(wall(Y1,X)), not(wall_num(Y1,X,_)), cellUp(Y1,X,L1), append([[Y1,X]],L1,L).
+
+cellUp(_,_,[]).
+
+cellDown(Y,X,L):-  Y1 is Y - 1, cell(Y1,X), not(wall(Y1,X)), not(wall_num(Y1,X,_)), cellDown(Y1,X,L1), append([[Y1,X]],L1,L).
+
+cellUp(_,_,[]).
+
+
+exRow(Y,X,L):-cellUp(Y,X,L1),cellDown(Y,X,L2), append(L1,L2,L).
